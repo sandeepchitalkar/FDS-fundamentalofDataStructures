b0c87e52c2b53ecfc9af83302ca294fc58ef2e9c
Sandeep Muktinath Chitalkar <111955719+sandeepchitalkar@users.noreply.github.com>
2023-09-08 11:07:00 +0530

    matrix.py

--- matrix.py
+++ matrix.py
@@ -1,8 +1,11 @@
+''' Write a Python program that takes two digits m (row) and n (column) as input and generates a two-dimensional array. 
+The element value in the i-th row and j-th column of the array should be i*j.'''
 #Program to read matrix and display on screen
 rows = int(input("Enter total number of rows:"))
 cols = int(input("Enter total number of Column:"))
-M1 = [[0 for j in range(cols)] for i in range(rows)] #initailze all value to 0
-M2 = [[0 for j in range(cols)] for i in range(rows)] #initailze all value to 0
+#initailze all value to 0 in matrix name arr
+arr = [[0 for j in range(cols)] for i in range(rows)] 
+
 #accepting a matrix from user
 for i in range(0,rows):
     for j in range(0,cols):
